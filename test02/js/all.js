f45acc1251070c11e9b4c2ff4d2f9c0b44620b37
chen-tse <a0975283413@gmail.com>
2022-05-19 15:11:12 +0800

    購物車驗證

--- test02/js/all.js
+++ test02/js/all.js
@@ -57,26 +57,105 @@ $(document).ready(function(){
     })
 
     // contact us page
-    document.querySelector("#contact_Name").oninvalid = function () {
+    $('#contact_Name').on('invalid',function(){
         if (!this.validity.valid) this.setCustomValidity("請輸入聯絡人名字!");
-    }
-    document.querySelector("#contact_Name").oninput = function () {
+    })
+    $('#contact_Name').on('input' ,function(){
         this.setCustomValidity("");
-    }
-    document.querySelector("#contact_Phone").oninput = function () {
+    })
+    $('#contact_Phone').on('input',function() {
         this.setCustomValidity("");
         let mystr = new RegExp('09[0-9]{8}');
         if (!mystr.test(this.value)) this.setCustomValidity("格式錯誤!例如:09xxxxxxxx");
-    }    
-    document.querySelector("#contact_Email").oninput = function () {
+    })    
+    $('#contact_Email').on('input' ,function() {
         this.setCustomValidity("");
         let mystr = new RegExp('[^@\s]+@[^@\s]+\.[^@\s]+');
         if (!mystr.test(this.value)) this.setCustomValidity("E-mail格式錯誤!");
-    }
-    document.querySelector("#contact_Info").oninvalid = function () {
+    })
+    $('#contact_Info').on('invalid' ,function(){
         if (!this.validity.valid) this.setCustomValidity("請輸入聯絡內容!");
-    }
-    document.querySelector("#contact_Info").oninput = function () {
+    })
+    $('#contact_Info').on('input' ,function() {
         this.setCustomValidity("");
-    }
+    })
+
+
+    // shopping car
+    $('.input_name').on('invalid',function(){
+        if (!this.validity.valid){
+            $(this).parent('.input-group').append(
+                $('<p/>').addClass('invalid-feedback')
+                .text('輸入姓名')
+            )
+        }
+    })
+    $('.input_phone').on('input',function(){
+        this.setCustomValidity("");
+        let mystr = new RegExp('09[0-9]{8}');
+        if (!mystr.test(this.value)) this.setCustomValidity("格式錯誤!例如:09xxxxxxxx");
+    })
+    $('.input_email').on('input' ,function() {
+        this.setCustomValidity("");
+        let mystr = new RegExp('[^@\s]+@[^@\s]+\.[^@\s]+');
+        if (!mystr.test(this.value)) this.setCustomValidity("E-mail格式錯誤!");
+    })
+
+    // shopping car page
+    $('.increase').on('click',function(){
+        var num = $('.piece').val();
+        console.log(num);
+        $('.decrease').prop('disabled', false);
+        num++;
+        flushSum(num);
+    })
+    $('.decrease').on('click',function(){
+        var num = $('.piece').val();
+        if(num <= 1){
+            this.prop('disabled', true);
+        }
+        num--;
+        flushSum(num);
+    })
+
+    $(".piece").change(function(){
+		//獲得輸入框的數量
+		var num = $(this).val();
+		//判斷是否輸入有誤
+		if(num == ""){
+			alert("輸入有誤");
+			num = 1;
+			$(this).val(1);
+		}
+		//重新整理小計
+		flushSum(num);
+	})
+    
+	function flushSum(num){
+		//重新整理商品數量
+		$(".piece").val(num);
+		//獲得商品的價格
+		var price = $(".product_count").text();
+		//轉型
+		price = parseFloat(price);
+
+        var freight = $('#freight').text();
+        freight = parseFloat(freight.substr(1));
+		//獲得商品小計
+		sum = num * price;
+        if( sum > 2000 ){
+            $('#freight').text("$" + 0 )
+            freight = 0;
+            $('#total').text("$" + (sum + freight));
+        }
+        else{
+            freight = 80;
+            $('#freight').text("$" + freight )
+        }
+		//重新整理商品數量價錢
+		$('.small_price').text(sum);
+        $('#totalPrice').text("$" + sum)
+        $("#totalCount").text("共" + num + "件");
+        $('#total').text("$" + (sum + freight));
+	}
 })
